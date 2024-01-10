2b4b104d873aecce2d53a8b76fe8f1a7d7d6b245
Noam Delmar <noamdelmar@h-MacBook-Air-sl-Noam.local>
2024-01-10 12:26:13 +0200

    working day 2

--- src/pages/ChatApp/ChatApp.jsx
+++ src/pages/ChatApp/ChatApp.jsx
@@ -1,64 +1,68 @@
-// src/Chat.js
 import React, { useState, useEffect } from 'react';
 import './styles.css';
 import { useLocation } from 'react-router-dom';
 import SideChats from '../../components/SideChats/SideChats';
+
 const Chat = () => {
   const [message, setMessage] = useState('');
   const [chatLog, setChatLog] = useState([]);
   const [socket, setSocket] = useState(null);
+  const [connectedUsers, setConnectedUsers] = useState([]);
   const location = useLocation();
-  const { username, port } = location.state;
+  const { username, port, room, password } = location.state;
 
-  const PORT = 8080
+  const PORT = 8080;
 
-useEffect(() => {
-  // Initialize WebSocket connection
-  const ws = new WebSocket(`ws://localhost:${PORT}`);
+  useEffect(() => {
+    // Initialize WebSocket connection
+    const ws = new WebSocket(`ws://localhost:${PORT}?room=${room}&password=${password}`);
 
-  ws.onopen = () => {
-    console.log('Connected to chat server');
+    ws.onopen = () => {
+      console.log('Connected to chat server');
 
-    // Send the username to the server
-    if (username.trim() !== '') {
-      const data = {
-        type: 'username',
-        username,
-      };
-      ws.send(JSON.stringify(data));
-    }
+      // Send the username to the server
+      if (username.trim() !== '') {
+        const data = {
+          type: 'username',
+          username,
+        };
+        ws.send(JSON.stringify(data));
+      }
 
-    // Set the WebSocket instance in the state
-    setSocket(ws);
-  };
+      // Set the WebSocket instance in the state
+      setSocket(ws);
+    };
 
-ws.onmessage = (event) => {
-  // Handle incoming messages
-  const dataString = event.data;
-  
-  // Extract the JSON content (remove the username prefix)
-  const jsonDataString = dataString.substring(dataString.indexOf(':') + 1).trim();
-  
-  try {
-    // Parse the JSON data
-    const data = JSON.parse(jsonDataString);
+    ws.onmessage = (event) => {
+      // Handle incoming messages
+      const dataString = event.data;
 
-    if (data.type === 'message') {
-      setChatLog((prevChatLog) => [...prevChatLog, {username: data.username, message: data.message}]);
-    }
-    // Add handling for other message types if needed
-  } catch (error) {
-    console.error('Error parsing JSON:', error);
-  }
-};
+      // Extract the JSON content (remove the username prefix)
+      const jsonDataString = dataString.substring(dataString.indexOf(':') + 1).trim();
 
-  return () => {
-    // Close WebSocket connection on component unmount
-    if (ws) {
-      ws.close();
-    }
-  };
-}, []);
+      try {
+        // Parse the JSON data
+        const data = JSON.parse(jsonDataString);
+
+        if (data.type === 'message') {
+          setChatLog((prevChatLog) => [...prevChatLog, { username: data.username, message: data.message }]);
+        }
+        if (data.type === 'userlist') {
+          setConnectedUsers(data.users);
+        }
+        // Add handling for other message types if needed
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    };
+
+    return () => {
+      // Close WebSocket connection on component unmount
+      if (ws) {
+        ws.close();
+      }
+    };
+  }, [username, room, password]);
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
@@ -73,28 +77,28 @@ ws.onmessage = (event) => {
         message,
       };
       socket.send(JSON.stringify(data));
-      setChatLog((prevChatLog) => [...prevChatLog, {username: username, message: message}]);
+      setChatLog((prevChatLog) => [...prevChatLog, { username, message }]);
       setMessage('');
     }
   };
 
   return (
-    <div style={{display: 'flex'}}>
-      <SideChats />
+    <div style={{ display: 'flex' }}>
+      <SideChats connectedUsers={connectedUsers} />
       <div className="chat-container">
         <div className="chat-log">
           {chatLog.map((chat, index) => (
-            <div key={index} className="chat-message" style={{alignSelf: username === chat.username ? 'self-end' : undefined}}>
+            <div
+              key={index}
+              className="chat-message"
+              style={{ alignSelf: username === chat.username ? 'self-end' : undefined }}
+            >
               {chat.username}: {chat.message}
             </div>
           ))}
         </div>
         <div className="input-container">
-          <input
-            type="text"
-            placeholder="Enter your username"
-            value={username}
-          />
+          <input type="text" placeholder="Enter your username" value={username} />
           <input
             type="text"
             placeholder="Type your message..."
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import BasicInput from '../../components/BasicInput/BasicInput';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [port, setPort] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [port, setPort] = useState(5050);
+  const [room, setRoom] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    return(
-        <div className='container'>
-            <div className='login-side'>
-                <h1>Welcome back!</h1>
-            </div>
-            <div className='login-container'>
-                <h1 className='login-title'>Login</h1>
-                <BasicInput name='Username' setValue={setUsername} />
-                <BasicInput name='Port' value={5050} setValue={setPort} />
-                <button className='login-button' disabled = {!username} onClick={() => navigate('/chat', { state: { username, port } })}>connect</button>
-            </div>
-        </div>
-    )
-}
+  const handleConnect = () => {
+    // Navigate to the chat page with the provided information
+    navigate('/chat', {
+      state: { username, port, room, password },
+    });
+  };
+
+  return (
+    <div className='container'>
+      <div className='login-side'>
+        <h1>Welcome back!</h1>
+      </div>
+      <div className='login-container'>
+        <h1 className='login-title'>Login</h1>
+        <BasicInput name='Username' setValue={setUsername} />
+        <BasicInput name='Port' value={port} setValue={setPort} />
+        <BasicInput name='Room' setValue={setRoom} />
+        <BasicInput name='Password' type='password' setValue={setPassword} />
+        <button className='login-button' disabled={!username || !room} onClick={handleConnect}>
+          Connect
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
